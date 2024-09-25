9b17ba5f3564805ac8428f6d4e911af838cc4505
John Mukhwana <Johnbradill67@gmail.com>
2024-09-25 15:16:06 +0300

    Added the useState to change background color

--- ReactTailwindcss/src/components/body.jsx
+++ ReactTailwindcss/src/components/body.jsx
@@ -1,9 +1,19 @@
 
+import {useState} from 'react';
 
 function Body(){
+    const[color , setColor] = useState("green");
+
+    const changeColor= () =>{
+        setColor(prevColor => prevColor ==="green"? "blue": "yellow");
+    };
     return (
-        <div className="container mx-auto p-4 bg-green-500 w-full h-80 item-center justify-center">
+        <div className="container mx-auto p-4  w-full h-80 item-center justify-center" style={{backgroundColor:color}} >
             <h1 className="text-2xl font-bold">Body</h1>
+            <button className="border "   onClick={changeColor} >
+                Click me
+               
+            </button>
             <p className="text-lg">This is the body of the page.</p>
         </div>
     );
