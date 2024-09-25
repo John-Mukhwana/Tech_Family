669b3de28133cd6ab7a667b89274c68976275a04
John Mukhwana <Johnbradill67@gmail.com>
2024-09-25 15:59:30 +0300

    Added the useReducer File

---
+++ ReactTailwindcss/src/components/useReducer.jsx
@@ -0,0 +1,54 @@
+
+
+import { useReducer } from "react";
+
+function reducer(state,action){
+    switch(action.type){
+        case 'incremented_age':{
+            return{
+                name: state.name,
+                age: state.age + 1
+
+            };
+        }
+        case 'changed_name':{
+            return{
+                name:action.nextName,
+                age: state.age
+            };
+        }
+    }
+    throw Error('Unknown actio:'+ action.type);
+}
+const initialState ={name:'Bradley', age: 21};
+
+export default function Form(){
+
+    const [state,dispatch]= useReducer(reducer,initialState);
+     
+    const handleButtonClick=()=>{
+       dispatch({type:'incremented_age'});
+    }
+
+    const handleInputChange=(e)=>{
+        dispatch({
+            type:'changed_name',
+            nectName:e.target.value
+        });
+    }
+
+    return(
+        <>
+        <input
+           value={state.name}
+           onChange={handleInputChange}
+        />
+        <button onClick={handleButtonClick}>
+            increment Age
+        </button>
+
+        <p>Hello,{state.name}.You are {state.age} Years Old</p>
+        </>
+    )
+    
+}
